53c209f725e2d9174600c81dea5e5b0a29c74594
afsharmiah <afsharmiah15@gmail.com>
2023-03-28 17:17:54 -0500

    Js and css files for counter-app

---
+++ Counter-app/react-counter-app/src/App.js
@@ -0,0 +1,36 @@
+import { React, useState } from 'react';
+import './App.css';
+
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+
+  //reset counter 
+  const reset = () => {
+    setCounter(0)
+  }
+
+  return (
+    <div className="counter">
+      <h1>React Counter</h1>
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={increase}>+</button>
+        <button className="control__btn" onClick={decrease}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
